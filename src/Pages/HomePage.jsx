84b18599eb9ccbd5d1e2e3886f82ea19e8208db0
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-10-30 23:34:24 +0600

    create login Form  usin react-hook-Form  also create header and fake login and logout system

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Header from "../Components/common/Header";
 
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <h2>Home page</h2>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
