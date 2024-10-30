01f8a3807ad6b73e591d5aee6ff19da6b6547ed3
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-10-30 16:31:23 +0600

    Create some pages and there router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
+import RegistrationPage from "./Pages/RegistrationPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/me" element={<ProfilePage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
